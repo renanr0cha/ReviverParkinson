5d890e7df8aa54f5aea3c026c6a9a2afa0ec3c3a
Renan Rocha <renan.aragorn@gmail.com>
2024-11-14 18:15:41 -0400

    remove test

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import { registerRootComponent } from 'expo';
 import 'expo-asset';
-import Constants from 'expo-constants';
 import App from './App';
-console.log(Constants.systemFonts);
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
